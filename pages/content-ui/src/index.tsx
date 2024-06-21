8f431dbba8d6e9908b9834e9da7ee713aca76a29
KET <ket0825@gmail.com>
2024-06-21 13:50:55 +0900

    feat: cover the whole pages

--- pages/content-ui/src/index.tsx
+++ pages/content-ui/src/index.tsx
@@ -1,23 +1,15 @@
 // index.tsx
 import { createRoot } from 'react-dom/client';
 import App from '@src/app';
-// index.tsx
-// eslint-disable-next-line
-// @ts-ignore
+
 import tailwindcssOutput from '@src/tailwind-output.css?inline';
 
-// const asideElement: HTMLElement | null = document.querySelector('[class^="style_aside__"]');
 
 const root = document.createElement('div');
 root.id = 'chrome-extension-boilerplate-react-vite-content-view-root';
 
-// if (!asideElement) {
-//   throw new Error('Could not find aside element');  
-// }
-
-// document.append(root);
-// asideElement?.appendChild(root);
 document.body.appendChild(root);
+
 
 const rootIntoShadow = document.createElement('div');
 rootIntoShadow.id = 'shadow-root';
--- pages/content-ui/tailwind.config.js
+++ pages/content-ui/tailwind.config.js
@@ -9,25 +9,18 @@ module.exports = {
     preflight: false,
   },
   // 이미 존재함.
-  // theme: {
-  //   extend: {
-  //     colors: {
-  //       gray: {
-  //         300: '#e2e8f0',
-  //         800: '#2d3748',
-  //         card: '#68717A',
-  //       },
-  //       blue: {
-  //         500: '#4299e1',
-  //       },
-  //       red: {
-  //         500: '#f56565',
-  //       },
-  //       green: {
-  //         500: '#48bb78',
-  //       },
-  //       black: '#000000',
-  //     },
-  //   },
-  // },
+  theme: {
+    extend: {
+      text: {
+        gray: {          
+          card: {
+            body: '#68717A'
+          },
+        },
+      },
+    },
+    fontFamily: {
+      Inter: ['Inter'],
+    },
+  },
 };
